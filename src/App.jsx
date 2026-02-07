eaac15005c78167288bd5463c014f9a3666d402d
muhammed sinan <muhammedsinanvavoor@gmail.com>
2026-02-07 17:01:50 +0530

    creat login page and set routing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Routes,Route } from 'react-router-dom'
 import Booking from './pages/Bookingpage'
+import Login from "./pages/loginPage"
 
 function App() {
 
   return (
-    <>
-     <Booking />
-    </>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/booking' element= {<Booking/>}/>
+    </Routes>
   )
 }
 
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,12 @@
+import LoginPage from "../components/login-page/login";
+
+function Login (){
+    return(
+
+    <>
+     <LoginPage />
+    </>
+    )
+}
+
+export default Login
